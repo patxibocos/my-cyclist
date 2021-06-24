ea28274b58bf72ce0a4154e6920079945bf451f7
Patxi Bocos <patxi952@gmail.com>
2021-06-24 12:42:49 +0200

    Initial Android app

---
+++ app/src/main/java/io/github/patxibocos/roadcyclingdata/MainActivity.kt
@@ -0,0 +1,62 @@
+package io.github.patxibocos.roadcyclingdata
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import io.github.patxibocos.roadcyclingdata.ui.theme.RoadCyclingDataTheme
+import java.util.Locale
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            RoadCyclingDataTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    CountryEmoji(Country("es"))
+                }
+            }
+        }
+    }
+}
+
+@JvmInline
+value class Country(private val code: String) {
+    init {
+        require(Locale.getISOCountries().contains(code.uppercase()))
+    }
+
+    fun code(): String {
+        return code.uppercase()
+    }
+}
+
+@Composable
+fun CountryEmoji(country: Country) {
+    Text(
+        text = "Hello ${getEmoji(country)}!",
+        fontSize = 30.sp,
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    RoadCyclingDataTheme {
+        CountryEmoji(Country("es"))
+    }
+}
+
+fun getEmoji(country: Country): String {
+    // Based on https://dev.to/jorik/country-code-to-flag-emoji-a21
+    val codePoints = country.code()
+        .map { char -> 127397 + char.toString().codePointAt(0) }
+        .toIntArray()
+    return String(codePoints, 0, codePoints.size)
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,11 @@
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "Road Cycling Data"
+include(":app")
+
+enableFeaturePreview("VERSION_CATALOGS")
