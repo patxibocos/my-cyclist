a910527d187a3e1c9b1e4bd0236b2ff0f8fcab71
Patxi Bocos <patxi952@gmail.com>
2021-07-03 11:32:27 +0200

    Cleaning 🧹

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,7 +2,6 @@ plugins {
     id("com.android.application")
     id("kotlin-android")
     id("kotlin-kapt")
-    kotlin("plugin.serialization") version "1.5.10"
 }
 
 android {
@@ -47,31 +46,29 @@ android {
 }
 
 dependencies {
-    kapt("androidx.room:room-compiler:2.3.0")
+    kapt("androidx.room:room-compiler:${libs.versions.room.get()}")
 
-    implementation("androidx.core:core-ktx:1.5.0")
+    implementation("androidx.core:core-ktx:1.6.0")
     implementation("androidx.appcompat:appcompat:1.3.0")
     implementation("com.google.android.material:material:1.3.0")
     implementation("androidx.compose.ui:ui:${libs.versions.compose.get()}")
     implementation("androidx.compose.material:material:${libs.versions.compose.get()}")
     implementation("androidx.compose.ui:ui-tooling:${libs.versions.compose.get()}")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.3.1")
-    implementation("androidx.activity:activity-compose:1.3.0-beta02")
-    implementation("androidx.room:room-runtime:2.3.0")
-    implementation("androidx.room:room-ktx:2.3.0")
-    implementation("androidx.work:work-runtime-ktx:2.6.0-beta01")
+    implementation("androidx.activity:activity-compose:1.3.0-rc01")
+    implementation("androidx.room:room-runtime:${libs.versions.room.get()}")
+    implementation("androidx.room:room-ktx:${libs.versions.room.get()}")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:1.0.0-alpha07")
     implementation("androidx.compose.runtime:runtime-livedata:${libs.versions.compose.get()}")
     implementation("androidx.paging:paging-runtime-ktx:3.0.0")
     implementation("androidx.paging:paging-compose:1.0.0-alpha10")
     implementation("com.google.accompanist:accompanist-coil:0.12.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.1")
 
     coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:1.1.5")
 
     testImplementation("junit:junit:4.13.2")
 
-    androidTestImplementation("androidx.test.ext:junit:1.1.2")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.3.0")
+    androidTestImplementation("androidx.test.ext:junit:1.1.3")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.4.0")
     androidTestImplementation("androidx.compose.ui:ui-test-junit4:${libs.versions.compose.get()}")
 }
--- app/src/main/java/io/github/patxibocos/roadcyclingdata/data/db/RiderDao.kt
+++ app/src/main/java/io/github/patxibocos/roadcyclingdata/data/db/RiderDao.kt
@@ -9,7 +9,4 @@ interface RiderDao {
     @Query("SELECT * FROM rider WHERE first_name LIKE '%' || :query || '%' COLLATE NOCASE OR last_name LIKE '%' || :query || '%' COLLATE NOCASE ORDER BY id ASC LIMIT :limit OFFSET :offset")
     suspend fun findRiders(query: String, limit: Int, offset: Int): List<Rider>
 
-    @Query("SELECT 1 WHERE 1 = 1")
-    fun init(): Int
-
 }
