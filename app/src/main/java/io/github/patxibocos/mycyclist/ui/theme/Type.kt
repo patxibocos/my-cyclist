c82a80cf143de4f992864949c0c980d36d9b8d7c
Patxi Bocos <patxi952@gmail.com>
2022-05-08 15:07:02 +0200

    Downloadable fonts

--- app/src/main/java/io/github/patxibocos/mycyclist/ui/theme/Type.kt
+++ app/src/main/java/io/github/patxibocos/mycyclist/ui/theme/Type.kt
@@ -1,123 +1,136 @@
 package io.github.patxibocos.mycyclist.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.text.ExperimentalTextApi
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.googlefonts.Font
+import androidx.compose.ui.text.googlefonts.GoogleFont
 import androidx.compose.ui.unit.sp
 import io.github.patxibocos.mycyclist.R
 
-private val roboto = FontFamily(
-    Font(R.font.roboto_thin, FontWeight.Thin),
-    Font(R.font.roboto_light, FontWeight.Light),
-    Font(R.font.roboto_regular, FontWeight.Normal),
-    Font(R.font.roboto_medium, FontWeight.Medium),
-    Font(R.font.roboto_bold, FontWeight.Bold),
-    Font(R.font.roboto_black, FontWeight.Black),
+@OptIn(ExperimentalTextApi::class)
+private val provider = GoogleFont.Provider(
+    providerAuthority = "com.google.android.gms.fonts",
+    providerPackage = "com.google.android.gms",
+    certificates = R.array.com_google_android_gms_fonts_certs
+)
+
+@OptIn(ExperimentalTextApi::class)
+private val robotoFont = GoogleFont(name = "Roboto")
+
+@OptIn(ExperimentalTextApi::class)
+private val robotoFontFamily = FontFamily(
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Thin),
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Light),
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Normal),
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Medium),
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Bold),
+    Font(googleFont = robotoFont, fontProvider = provider, weight = FontWeight.Black),
 )
 
 val AppTypography = Typography(
     displayLarge = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 57.sp,
         lineHeight = 64.sp,
         letterSpacing = (-0.25).sp,
     ),
     displayMedium = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 45.sp,
         lineHeight = 52.sp,
         letterSpacing = 0.sp,
     ),
     displaySmall = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 36.sp,
         lineHeight = 44.sp,
         letterSpacing = 0.sp,
     ),
     headlineLarge = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 32.sp,
         lineHeight = 40.sp,
         letterSpacing = 0.sp,
     ),
     headlineMedium = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 28.sp,
         lineHeight = 36.sp,
         letterSpacing = 0.sp,
     ),
     headlineSmall = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 24.sp,
         lineHeight = 32.sp,
         letterSpacing = 0.sp,
     ),
     titleLarge = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 22.sp,
         lineHeight = 28.sp,
         letterSpacing = 0.sp,
     ),
     titleMedium = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.1.sp,
     ),
     titleSmall = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
     ),
     labelLarge = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.1.sp,
     ),
     bodyLarge = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 16.sp,
         lineHeight = 24.sp,
         letterSpacing = 0.5.sp,
     ),
     bodyMedium = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 14.sp,
         lineHeight = 20.sp,
         letterSpacing = 0.25.sp,
     ),
     bodySmall = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.W400,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.4.sp,
     ),
     labelMedium = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 12.sp,
         lineHeight = 16.sp,
         letterSpacing = 0.5.sp,
     ),
     labelSmall = TextStyle(
-        fontFamily = roboto,
+        fontFamily = robotoFontFamily,
         fontWeight = FontWeight.Medium,
         fontSize = 11.sp,
         lineHeight = 16.sp,
