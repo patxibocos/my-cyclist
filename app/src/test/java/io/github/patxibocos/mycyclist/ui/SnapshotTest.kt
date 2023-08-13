6b465285b84870c35dbf7600fdcaaa1e982f0665
Patxi Bocos <patxibocos@lyft.com>
2023-08-13 06:23:17 -0700

    remove a test

--- app/src/test/java/io/github/patxibocos/mycyclist/ui/SnapshotTest.kt
+++ app/src/test/java/io/github/patxibocos/mycyclist/ui/SnapshotTest.kt
@@ -2,9 +2,7 @@ package io.github.patxibocos.mycyclist.ui
 
 import app.cash.paparazzi.DeviceConfig
 import app.cash.paparazzi.Paparazzi
-import io.github.patxibocos.mycyclist.ui.preview.RacePreview
 import io.github.patxibocos.mycyclist.ui.preview.RiderPreview
-import io.github.patxibocos.mycyclist.ui.preview.TeamPreview
 import org.junit.Rule
 import org.junit.Test
 
@@ -19,20 +17,6 @@ class SnapshotTest {
     fun rider() {
         paparazzi.snapshot {
             RiderPreview()
-        }
-    }
-
-    @Test
-    fun team() {
-        paparazzi.snapshot {
-            TeamPreview()
-        }
-    }
-
-    @Test
-    fun race() {
-        paparazzi.snapshot {
-            RacePreview()
         }
     }
 }
